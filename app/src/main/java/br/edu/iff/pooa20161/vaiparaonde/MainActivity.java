2a9a9e40d27b8c04514a3354f444d2ea49737bf1
Luiz Gustavo <lglmoura@gmail.com>
2016-08-02 11:57:50 -0300

    Criando Menu no  Activity Principal

--- app/src/main/java/br/edu/iff/pooa20161/vaiparaonde/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20161/vaiparaonde/MainActivity.java
@@ -1,11 +1,12 @@
 package br.edu.iff.pooa20161.vaiparaonde;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,36 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        if (id == R.id.action_Evento) {
+            Intent intent = new Intent(getContext(), EventoActivity.class);
+            startActivity(intent);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Context getContext() {
+        return this;
     }
 
 }
